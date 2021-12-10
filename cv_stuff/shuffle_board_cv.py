b8982c405cadfd2fe5baab03da16ae091cacad56
Class Account <ee106a-aew@c105-9.local>
2021-12-09 16:01:19 -0800

    mouse1

--- cv_stuff/shuffle_board_cv.py
+++ cv_stuff/shuffle_board_cv.py
@@ -15,11 +15,11 @@ def get_real_board_state(length, width, pixel_height=800, pixel_width=150):
         img = getImage()
         corner_points = homoFromMouse(img)
     
-    # for corner in corner_points:
-    #     cv2.circle(img, tuple(corner), radius=0, color=(0, 0, 255), thickness=10)
-    # imgS = cv2.resize(img, (960, 540))
-    # cv2.imshow("circled", img)
-    # cv2.waitKey(0)
+    for corner in corner_points:
+        cv2.circle(img, tuple(corner), radius=0, color=(0, 0, 255), thickness=10)
+    imgS = cv2.resize(img, (960, 540))
+    cv2.imshow("circled", img)
+    cv2.waitKey(0)
     # print(corner_points)
     warped = four_point_transform(img, corner_points, pixel_height, pixel_width)
     # warpedS = cv2.resize(warped, (150, 800))
